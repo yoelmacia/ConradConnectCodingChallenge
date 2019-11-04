2d9d9500e59f509d43ab5b4e55363274055251ed
YoelMacia <yoelmaciadelgado@gmail.com>
2019-11-04 08:55:48 +0100

    Add search endpoint
    Create one endpoint to allow to search a term
    and returns a list of repositories.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const fetch = require("node-fetch");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/api/repo", (req, res) => {
+  const data = [];
+  const url = "https://api.github.com/search/repositories?q=tetris";
+  const getData = async url => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      data.push(json);
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getData(url);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
