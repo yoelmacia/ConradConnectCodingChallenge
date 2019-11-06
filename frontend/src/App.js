64a356e2938a00b4c6ade75c1450062011f6a95b
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2019-11-06 21:33:21 +0100

    Welcome react component
    Add a welcome component with an automatic
    login button with github oauth2.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./App.css";
+import Welcome from "./components/Welcome";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Welcome.css";
+
+export default class Welcome extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="container-div">Welcome Github API</div>
+        <button className="container-button">
+          <a
+            className="container-link"
+            href="https://github.com/login/oauth/authorize?client_id=723d03ab329e99ca2336&scope=public_repo"
+          >
+            Sign up with github
+          </a>
+        </button>
+      </div>
+    );
+  }
+}
