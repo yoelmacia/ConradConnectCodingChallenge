adc94517fd5ef2768f087726f5777cf1f13f4db9
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2019-11-07 10:59:35 +0100

    Add react route system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./App.css";
 import Welcome from "./components/Welcome";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Welcome />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
